81b4fe763d3ae6c71cad246e464ff8655e565901
amna37h <amna37h@gmail.com>
2023-05-09 13:50:44 +0530

    NavBar added

---
+++ frontend/review-app/src/components/NavBar.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="absolute" style={{background: '#c61a09'}}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Book Reviewer
+          </Typography>
+          <Button color="inherit">Home</Button>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
---
+++ frontend/review-app/src/components/ProductDisplayed.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+
+
+
+
+export default function ProductDisplayed() {
+
+
+
+
+
+
+}
+
+
+
